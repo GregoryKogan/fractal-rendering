2bb4824574deba48c7fb788faa0a5950b04c4cf6
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-10-29 17:32:16 +0300

    Julia set rendering added

--- fractal-rendering/src/cpp/src/Sketch/Sketch.cpp
+++ fractal-rendering/src/cpp/src/Sketch/Sketch.cpp
@@ -2,48 +2,32 @@
 
 Sketch::Sketch(SDL_Renderer* renderer) : renderer_(renderer) { setup(); }
 
-void Sketch::setup() {
-    // random block position and velocity
-    block_x_ = rand() % window_width_;
-    block_y_ = rand() % window_height_;
-    block_vel_x_ = (rand() % 1000) / 2000.0;
-    block_vel_y_ = (rand() % 1000) / 2000.0;
-}
+void Sketch::setup() {}
 
 void Sketch::update(double delta_time) {
-    block_x_ += block_vel_x_ * delta_time;
-    block_y_ += block_vel_y_ * delta_time;
-
-    if (block_x_ < 0) {
-        block_x_ = 0;
-        block_vel_x_ *= -1;
-    } else if (block_x_ + block_w_ > window_width_) {
-        block_x_ = window_width_ - block_w_;
-        block_vel_x_ *= -1;
-    }
-
-    if (block_y_ < 0) {
-        block_y_ = 0;
-        block_vel_y_ *= -1;
-    } else if (block_y_ + block_h_ > window_height_) {
-        block_y_ = window_height_ - block_h_;
-        block_vel_y_ *= -1;
-    }
+    animation_progress_ += animation_speed_ * delta_time;
+    if (animation_progress_ >= 1.0f) animation_progress_ = 0.0f;
+    calculate_c_();
 }
 
 void Sketch::draw() const noexcept {
-    SDL_SetRenderDrawColor(renderer_, std::get<0>(bg_color_), std::get<1>(bg_color_), std::get<2>(bg_color_), 255);
+    SDL_SetRenderDrawColor(renderer_, 0, 0, 0, 255);
     SDL_RenderClear(renderer_);
 
-    SDL_Rect rect;
-    rect.x = block_x_;
-    rect.y = block_y_;
-    rect.w = block_w_;
-    rect.h = block_h_;
+    int cell_width = std::max(std::min(window_width_, window_height_) / resolution_, 1);
 
-    SDL_SetRenderDrawColor(renderer_, 169, 177, 214, 255);
-    SDL_RenderFillRect(renderer_, &rect);
-    SDL_SetRenderDrawColor(renderer_, std::get<0>(bg_color_), std::get<1>(bg_color_), std::get<2>(bg_color_), 255);
+    for (int x = 0; x <= window_width_ / cell_width; ++x) {
+        for (int y = 0; y <= window_height_ / cell_width; ++y) {
+            float scaled_x = std::lerp(-2.0, 2.0, float(x) / (window_width_ / cell_width));
+            float scaled_y = std::lerp(-2.0, 2.0, float(y) / (window_height_ / cell_width));
+            float iters = is_in_set_(scaled_x, scaled_y);
+
+            int brightness = 255 * iters / max_iterations_;
+            SDL_SetRenderDrawColor(renderer_, brightness, brightness, brightness, 255);
+            SDL_Rect rect{x * cell_width, y * cell_width, cell_width, cell_width};
+            SDL_RenderFillRect(renderer_, &rect);
+        }
+    }
 
     SDL_RenderPresent(renderer_);
 }
@@ -51,4 +35,29 @@ void Sketch::draw() const noexcept {
 void Sketch::set_window_size(int width, int height) noexcept {
     window_width_ = width;
     window_height_ = height;
+}
+
+void Sketch::calculate_c_() noexcept {
+    int index = int(animation_progress_ * (c_points_.size() - 1));
+    float progress = std::fmod(animation_progress_ * (c_points_.size() - 1), 1.0f);
+
+    float c_real = std::lerp(c_points_[index].real(), c_points_[index + 1].real(), progress);
+    float c_imag = std::lerp(c_points_[index].imag(), c_points_[index + 1].imag(), progress);
+    c_ = std::complex<float>(c_real, c_imag);
+}
+
+float Sketch::is_in_set_(float x, float y) const noexcept {
+    std::complex<float> z(x, y);
+
+    int iterations = 0;
+    while (++iterations < max_iterations_) {
+        z = static_cast<std::complex<float>>(std::pow(z, 2)) + c_;
+        if (std::norm(z) > 4) break;
+    }
+
+    if (iterations < 2) return 0.0f;
+
+    float mod = std::sqrt(std::norm(z));
+    float smooth = float(iterations) - std::log2(std::max(1.0f, std::log2(mod)));
+    return smooth;
 }
--- fractal-rendering/src/cpp/src/Sketch/Sketch.hpp
+++ fractal-rendering/src/cpp/src/Sketch/Sketch.hpp
@@ -3,7 +3,9 @@
 
 #include <SDL2/SDL.h>
 
-#include <tuple>
+#include <cmath>
+#include <complex>
+#include <numeric>
 
 class Sketch {
    public:
@@ -20,14 +22,22 @@ class Sketch {
     int window_width_ = 1024;
     int window_height_ = 1024;
 
-    std::tuple<int, int, int> bg_color_ = {36, 40, 59};
+    int resolution_ = 256;
+    int max_iterations_ = 30;
+    std::complex<float> c_ = std::complex<float>(0.0f, 0.0f);
 
-    double block_x_ = 0;
-    double block_y_ = 0;
-    double block_w_ = 50;
-    double block_h_ = 50;
-    double block_vel_x_ = 0;
-    double block_vel_y_ = 0;
+    // interpolation animation for c_ between multiple points
+    std::vector<std::complex<float>> c_points_ = {
+        std::complex<float>(0.0f, 0.0f),    std::complex<float>(-0.4f, -0.59f),     std::complex<float>(-0.54f, 0.54f),
+        std::complex<float>(-0.8f, 0.156f), std::complex<float>(-0.835f, -0.2321f), std::complex<float>(0.0f, 0.8f),
+        std::complex<float>(0.285f, 0.01f), std::complex<float>(0.34f, -0.05f),     std::complex<float>(0.355f, 0.355f),
+        std::complex<float>(0.37f, 0.1f),   std::complex<float>(0.0f, 0.0f)};
+    float animation_progress_ = 0.0f;
+    float animation_speed_ = 0.00003f;
+
+    void calculate_c_() noexcept;
+
+    float is_in_set_(float x, float y) const noexcept;
 };
 
 #endif
