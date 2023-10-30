62cfb462323bdd322192bb87f4358ce577ad8ee2
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-10-30 23:31:09 +0300

    refactoring

--- fractal-rendering/src/cpp/src/Sketch/Sketch.cpp
+++ fractal-rendering/src/cpp/src/Sketch/Sketch.cpp
@@ -8,8 +8,7 @@ void Sketch::setup() {
     grid_width_ = resolution_ / 2;
     scaled_x_step_ = 2.0f / (grid_width_ - 1) * render_step_width_;
     scaled_y_step_ = 4.0f / (grid_height_ - 1) * render_step_width_;
-    grid_.resize(grid_height_);
-    for (auto& row : grid_) row.resize(grid_width_, 0.0f);
+    grid_.resize(grid_height_, std::vector<float>(grid_width_, 0.0f));
     canvas_width_ = cell_width_ * grid_height_;
     canvas_center_ = canvas_width_ / 2;
     canvas_offset_x_ = (window_width_ - canvas_width_) / 2;
@@ -36,13 +35,7 @@ void Sketch::draw() noexcept {
 
             const auto [r, g, b] = get_color_(t);
             SDL_SetRenderDrawColor(renderer_, r, g, b, 255);
-            SDL_Rect rect{canvas_offset_x_ + canvas_center_ + x * cell_width_, canvas_offset_y_ + y * cell_width_,
-                          cell_width_, cell_width_};
-            SDL_Rect rect_mirror{canvas_offset_x_ + canvas_center_ - x * cell_width_ - cell_width_,
-                                 canvas_offset_y_ + canvas_width_ - y * cell_width_ - cell_width_, cell_width_,
-                                 cell_width_};
-            SDL_RenderFillRect(renderer_, &rect);
-            SDL_RenderFillRect(renderer_, &rect_mirror);
+            draw_rect_(x, y);
         }
     }
 
@@ -52,156 +45,207 @@ void Sketch::draw() noexcept {
 void Sketch::set_window_size(const int& width, const int& height) noexcept {
     window_width_ = width;
     window_height_ = height;
-
     setup();
 }
 
 void Sketch::render_grid_() noexcept {
-    // sparse rendering
-    int x = 0;
-    float scaled_x = 0.0f;
-    while (x < grid_width_) {
-        int y = 0;
-        float scaled_y = -2.0f;
-        while (y < grid_height_) {
-            grid_[y][x] = is_in_set_(scaled_x, scaled_y);
+    render_sparse_grid();
 
-            y += render_step_width_;
-            scaled_y += scaled_y_step_;
-        }
-        x += render_step_width_;
-        scaled_x += scaled_x_step_;
-    }
-
-    // Precompute scaled_x_step_ / render_step_width_ and scaled_y_step_ / render_step_width_
     const float scaled_x_step_div_render_step_width = scaled_x_step_ / render_step_width_;
     const float scaled_y_step_div_render_step_width = scaled_y_step_ / render_step_width_;
 
-    for (x = 0; x < grid_width_ - render_step_width_; x += render_step_width_) {
+    for (int x = 0; x < grid_width_ - render_step_width_; x += render_step_width_) {
         for (int y = 0; y < grid_height_ - render_step_width_; y += render_step_width_) {
-            // skip if all corners are black
-            if ((grid_[y][x] == 0.0f && grid_[y][x + render_step_width_] == 0.0f &&
-                 grid_[y + render_step_width_][x] == 0.0f &&
-                 grid_[y + render_step_width_][x + render_step_width_] == 0.0f) ||
-                (grid_[y][x] == 1.0f && grid_[y][x + render_step_width_] == 1.0f &&
-                 grid_[y + render_step_width_][x] == 1.0f &&
-                 grid_[y + render_step_width_][x + render_step_width_] == 1.0f))
-                continue;
+            if (are_corners_black(x, y)) continue;
 
-            // fill in the 4 edges
-            for (int i = 1; i < render_step_width_; i++) {
-                grid_[y][x + i] = is_in_set_(scaled_x_step_div_render_step_width * (x + i),
-                                             scaled_y_step_div_render_step_width * y - 2.0f);
-                grid_[y + render_step_width_][x + i] =
-                    is_in_set_(scaled_x_step_div_render_step_width * (x + i),
-                               scaled_y_step_div_render_step_width * (y + render_step_width_) - 2.0f);
-                grid_[y + i][x] = is_in_set_(scaled_x_step_div_render_step_width * x,
-                                             scaled_y_step_div_render_step_width * (y + i) - 2.0f);
-                grid_[y + i][x + render_step_width_] =
-                    is_in_set_(scaled_x_step_div_render_step_width * (x + render_step_width_),
-                               scaled_y_step_div_render_step_width * (y + i) - 2.0f);
-            }
-
-            // fill in checkerboard pattern (split into 2 loops to avoid branching)
-            for (int i = 1; i < render_step_width_; i += 2) {
-                for (int j = 1; j < render_step_width_; j += 2) {
-                    grid_[y + i][x + j] = is_in_set_(scaled_x_step_div_render_step_width * (x + j),
-                                                     scaled_y_step_div_render_step_width * (y + i) - 2.0f);
-                }
-            }
-            for (int i = 2; i < render_step_width_; i += 2) {
-                for (int j = 2; j < render_step_width_; j += 2) {
-                    grid_[y + i][x + j] = is_in_set_(scaled_x_step_div_render_step_width * (x + j),
-                                                     scaled_y_step_div_render_step_width * (y + i) - 2.0f);
-                }
-            }
-
-            // fill in the rest by averaging the 4 neighbors (split into 2 loops to avoid branching)
-            for (int i = 1; i < render_step_width_; i += 2) {
-                for (int j = 2; j < render_step_width_; j += 2) {
-                    grid_[y + i][x + j] = (grid_[y + i - 1][x + j] + grid_[y + i + 1][x + j] + grid_[y + i][x + j - 1] +
-                                           grid_[y + i][x + j + 1]) /
-                                          4.0f;
-                }
-            }
-            for (int i = 2; i < render_step_width_; i += 2) {
-                for (int j = 1; j < render_step_width_; j += 2) {
-                    grid_[y + i][x + j] = (grid_[y + i - 1][x + j] + grid_[y + i + 1][x + j] + grid_[y + i][x + j - 1] +
-                                           grid_[y + i][x + j + 1]) /
-                                          4.0f;
-                }
-            }
+            fill_edges(x, y, scaled_x_step_div_render_step_width, scaled_y_step_div_render_step_width);
+            fill_checkerboard_pattern(x, y, scaled_x_step_div_render_step_width, scaled_y_step_div_render_step_width);
+            fill_rest_by_averaging_neighbors(x, y);
         }
     }
 }
 
-inline void Sketch::calculate_c_() noexcept {
-    const int index = static_cast<int>(animation_progress_ * (c_points_.size() - 1));
-    const float progress = std::fmod(animation_progress_ * (c_points_.size() - 1), 1.0f);
-
-    c_real_ = c_points_[index].first + (c_points_[index + 1].first - c_points_[index].first) * progress;
-    c_imag_ = c_points_[index].second + (c_points_[index + 1].second - c_points_[index].second) * progress;
+void Sketch::render_sparse_grid() noexcept {
+    int x = 0;
+    float scaled_x = 0.0f;
+    while (x < grid_width_) {
+        render_sparse_grid_column(x, scaled_x);
+        x += render_step_width_;
+        scaled_x += scaled_x_step_;
+    }
 }
 
+void Sketch::render_sparse_grid_column(int x, float scaled_x) noexcept {
+    int y = 0;
+    float scaled_y = -2.0f;
+    while (y < grid_height_) {
+        grid_[y][x] = is_in_set_(scaled_x, scaled_y);
+        y += render_step_width_;
+        scaled_y += scaled_y_step_;
+    }
+}
+
+bool Sketch::are_corners_black(int x, int y) noexcept {
+    return (grid_[y][x] == 0.0f && grid_[y][x + render_step_width_] == 0.0f &&
+            grid_[y + render_step_width_][x] == 0.0f &&
+            grid_[y + render_step_width_][x + render_step_width_] == 0.0f) ||
+           (grid_[y][x] == 1.0f && grid_[y][x + render_step_width_] == 1.0f &&
+            grid_[y + render_step_width_][x] == 1.0f && grid_[y + render_step_width_][x + render_step_width_] == 1.0f);
+}
+
+void Sketch::fill_edges(int x, int y, float scaled_x_step_div_render_step_width,
+                        float scaled_y_step_div_render_step_width) noexcept {
+    for (int i = 1; i < render_step_width_; i++) {
+        fill_edge(x, y, i, scaled_x_step_div_render_step_width, scaled_y_step_div_render_step_width);
+    }
+}
+
+void Sketch::fill_edge(int x, int y, int i, float scaled_x_step_div_render_step_width,
+                       float scaled_y_step_div_render_step_width) noexcept {
+    grid_[y][x + i] =
+        is_in_set_(scaled_x_step_div_render_step_width * (x + i), scaled_y_step_div_render_step_width * y - 2.0f);
+    grid_[y + render_step_width_][x + i] =
+        is_in_set_(scaled_x_step_div_render_step_width * (x + i),
+                   scaled_y_step_div_render_step_width * (y + render_step_width_) - 2.0f);
+    grid_[y + i][x] =
+        is_in_set_(scaled_x_step_div_render_step_width * x, scaled_y_step_div_render_step_width * (y + i) - 2.0f);
+    grid_[y + i][x + render_step_width_] = is_in_set_(scaled_x_step_div_render_step_width * (x + render_step_width_),
+                                                      scaled_y_step_div_render_step_width * (y + i) - 2.0f);
+}
+
+void Sketch::fill_checkerboard_pattern(int x, int y, float scaled_x_step_div_render_step_width,
+                                       float scaled_y_step_div_render_step_width) noexcept {
+    for (int i = 1; i < render_step_width_; i += 2) {
+        for (int j = 1; j < render_step_width_; j += 2) {
+            grid_[y + i][x + j] = is_in_set_(scaled_x_step_div_render_step_width * (x + j),
+                                             scaled_y_step_div_render_step_width * (y + i) - 2.0f);
+        }
+    }
+    for (int i = 2; i < render_step_width_; i += 2) {
+        for (int j = 2; j < render_step_width_; j += 2) {
+            grid_[y + i][x + j] = is_in_set_(scaled_x_step_div_render_step_width * (x + j),
+                                             scaled_y_step_div_render_step_width * (y + i) - 2.0f);
+        }
+    }
+}
+
+void Sketch::fill_rest_by_averaging_neighbors(int x, int y) noexcept {
+    for (int i = 1; i < render_step_width_; i += 2) {
+        for (int j = 2; j < render_step_width_; j += 2) {
+            grid_[y + i][x + j] = average_of_neighbors(x, y, i, j);
+        }
+    }
+    for (int i = 2; i < render_step_width_; i += 2) {
+        for (int j = 1; j < render_step_width_; j += 2) {
+            grid_[y + i][x + j] = average_of_neighbors(x, y, i, j);
+        }
+    }
+}
+
+float Sketch::average_of_neighbors(int x, int y, int i, int j) noexcept {
+    return (grid_[y + i - 1][x + j] + grid_[y + i + 1][x + j] + grid_[y + i][x + j - 1] + grid_[y + i][x + j + 1]) /
+           4.0f;
+}
+
+inline void Sketch::calculate_c_() noexcept {
+    const size_t index = static_cast<size_t>(animation_progress_ * (c_points_.size() - 1));
+    const float progress = std::fmod(animation_progress_ * (c_points_.size() - 1), 1.0f);
+
+    const auto& current_point = c_points_[index];
+    const auto& next_point = c_points_[index + 1];
+
+    c_real_ = current_point.first + (next_point.first - current_point.first) * progress;
+    c_imag_ = current_point.second + (next_point.second - current_point.second) * progress;
+}
+
+// This function checks if a given point (x, y) is in the Julia set
 inline float Sketch::is_in_set_(const float& x, const float& y) const noexcept {
+    // Initialize constants for the real and imaginary parts of the complex constant c
     const v128_t c_real = wasm_f32x4_splat(c_real_);
     const v128_t c_imag = wasm_f32x4_splat(c_imag_);
+
+    // Initialize constants for the number 2 and -2
     const v128_t two = wasm_f32x4_splat(2.0f);
     const v128_t neg_two = wasm_f32x4_splat(-2.0f);
-    const v128_t one = wasm_f32x4_splat(1.0f);
-    const v128_t zero = wasm_f32x4_splat(0.0f);
+
+    // Initialize the maximum number of iterations
     const v128_t max_iterations = wasm_f32x4_splat(static_cast<float>(max_iterations_));
 
+    // Initialize the real and imaginary parts of z
     v128_t z_real = wasm_f32x4_splat(x);
     v128_t z_imag = wasm_f32x4_splat(y);
 
+    // Initialize the iteration counter
     int iterations = 0;
+
+    // Iterate until the maximum number of iterations is reached
     while (++iterations < max_iterations_) {
+        // Calculate the squares of the real and imaginary parts of z
         const v128_t z_real_squared = wasm_f32x4_mul(z_real, z_real);
         const v128_t z_imag_squared = wasm_f32x4_mul(z_imag, z_imag);
+
+        // Update the real part of z
         v128_t z_real_temp = wasm_f32x4_sub(z_real_squared, z_imag_squared);
         z_real_temp = wasm_f32x4_add(z_real_temp, c_real);
+
+        // Update the imaginary part of z
         z_imag = wasm_f32x4_mul(wasm_f32x4_mul(z_real, z_imag), two);
         z_imag = wasm_f32x4_add(z_imag, c_imag);
+
+        // Update the real part of z
         z_real = z_real_temp;
 
-        const v128_t z_real_gt_two = wasm_f32x4_gt(z_real, two);
-        const v128_t z_real_lt_neg_two = wasm_f32x4_lt(z_real, neg_two);
-        const v128_t z_imag_gt_two = wasm_f32x4_gt(z_imag, two);
-        const v128_t z_imag_lt_neg_two = wasm_f32x4_lt(z_imag, neg_two);
-        bool any_out_of_bounds = wasm_v128_any_true(wasm_v128_or(wasm_v128_or(z_real_gt_two, z_real_lt_neg_two),
-                                                                 wasm_v128_or(z_imag_gt_two, z_imag_lt_neg_two)));
-        if (any_out_of_bounds) break;
+        // Check if z has escaped the circle of radius 2
+        const v128_t out_of_bounds =
+            wasm_v128_or(wasm_v128_or(wasm_f32x4_gt(z_real, two), wasm_f32x4_lt(z_real, neg_two)),
+                         wasm_v128_or(wasm_f32x4_gt(z_imag, two), wasm_f32x4_lt(z_imag, neg_two)));
+
+        // If z has escaped, break the loop
+        if (wasm_v128_any_true(out_of_bounds)) break;
     }
 
+    // If only one iteration was done, return 0
     if (iterations == 1) return 0.0f;
 
-    const v128_t z_real_squared = wasm_f32x4_mul(z_real, z_real);
-    const v128_t z_imag_squared = wasm_f32x4_mul(z_imag, z_imag);
-    const v128_t mod_squared = wasm_f32x4_add(z_real_squared, z_imag_squared);
+    // Calculate the modulus of z
+    const v128_t mod_squared = wasm_f32x4_add(wasm_f32x4_mul(z_real, z_real), wasm_f32x4_mul(z_imag, z_imag));
     const v128_t mod = wasm_f32x4_sqrt(mod_squared);
 
+    // Calculate the average modulus
     const float scalar_mod = (wasm_f32x4_extract_lane(mod, 0) + wasm_f32x4_extract_lane(mod, 1) +
                               wasm_f32x4_extract_lane(mod, 2) + wasm_f32x4_extract_lane(mod, 3)) /
                              4.0f;
 
+    // Calculate the logarithm of the average modulus
     const float log_scalar_mod = fast_log2(std::max(1.0f, fast_log2(scalar_mod)));
+
+    // Calculate the smooth color
     const float smooth = static_cast<float>(iterations) - log_scalar_mod;
     const float t = smooth / static_cast<float>(max_iterations_);
 
-    // if t is close to 1.0f or 0.0f, then make it exactly 1.0f or 0.0f
-    if (t > 0.999f) return 1.0f;
-    if (t < 0.001f) return 0.0f;
-
-    return t;
+    // If t is close to 1.0f or 0.0f, then make it exactly 1.0f or 0.0f
+    return (t > 0.999f) ? 1.0f : ((t < 0.001f) ? 0.0f : t);
 }
 
 inline std::tuple<int, int, int> Sketch::get_color_(const float& t) const noexcept {
-    const int r = 9 * (1 - t) * t * t * t * 255;
-    const int g = 15 * (1 - t) * (1 - t) * t * t * 255;
-    const int b = 8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255;
+    const float inverse_t = 1 - t;
+    const float t_squared = t * t;
+    const float t_cubed = t_squared * t;
 
-    return std::tuple<int, int, int>(r, g, b);
+    const int r = static_cast<int>(9 * inverse_t * t_cubed * 255);
+    const int g = static_cast<int>(15 * inverse_t * inverse_t * t_squared * 255);
+    const int b = static_cast<int>(8.5 * inverse_t * inverse_t * inverse_t * t * 255);
+
+    return std::make_tuple(r, g, b);
+}
+
+void Sketch::draw_rect_(const int& x, const int& y) {
+    SDL_Rect rect{canvas_offset_x_ + canvas_center_ + x * cell_width_, canvas_offset_y_ + y * cell_width_, cell_width_,
+                  cell_width_};
+    SDL_Rect rect_mirror{canvas_offset_x_ + canvas_center_ - x * cell_width_ - cell_width_,
+                         canvas_offset_y_ + canvas_width_ - y * cell_width_ - cell_width_, cell_width_, cell_width_};
+    SDL_RenderFillRect(renderer_, &rect);
+    SDL_RenderFillRect(renderer_, &rect_mirror);
 }
 
 inline float fast_log2(float x) {
@@ -213,8 +257,7 @@ inline float fast_log2(float x) {
         uint32_t i;
         float f;
     } mx = {(vx.i & 0x007FFFFF) | 0x3f000000};
-    float y = vx.i;
-    y *= 1.1920928955078125e-7f;
 
+    float y = vx.i * 1.1920928955078125e-7f;
     return y - 124.22551499f - 1.498030302f * mx.f - 1.72587999f / (0.3520887068f + mx.f);
 }
--- fractal-rendering/src/cpp/src/Sketch/Sketch.hpp
+++ fractal-rendering/src/cpp/src/Sketch/Sketch.hpp
@@ -15,48 +15,57 @@ class Sketch {
     void setup();
     void update(const double &delta_time);
     void draw() noexcept;
-
     void set_window_size(const int &width, const int &height) noexcept;
 
    private:
     SDL_Renderer *renderer_;
     int window_width_ = 0;
     int window_height_ = 0;
+
     int canvas_width_ = 0;
     int canvas_center_ = 0;
     int canvas_offset_x_ = 0;
     int canvas_offset_y_ = 0;
+
     int grid_height_ = 0;
     int grid_width_ = 0;
+    std::vector<std::vector<float>> grid_;
 
     int resolution_ = 512;
     int render_step_width_ = 6;
     int cell_width_ = 0;
     float scaled_x_step_ = 0.0f;
     float scaled_y_step_ = 0.0f;
-    std::vector<std::vector<float>> grid_;
 
     int max_iterations_ = 50;
     float c_real_ = 0.0f;
     float c_imag_ = 0.0f;
 
-    void render_grid_() noexcept;
-
     // interpolation animation for c_ between multiple points
-    std::vector<std::pair<float, float>> c_points_ = {
-        std::pair<float, float>(0.0f, 0.0f),        std::pair<float, float>(-0.4f, -0.59f),
-        std::pair<float, float>(-0.54f, 0.54f),     std::pair<float, float>(-0.8f, 0.156f),
-        std::pair<float, float>(-0.835f, -0.2321f), std::pair<float, float>(0.0f, 0.8f),
-        std::pair<float, float>(0.285f, 0.01f),     std::pair<float, float>(0.34f, -0.05f),
-        std::pair<float, float>(0.355f, 0.355f),    std::pair<float, float>(0.37f, 0.1f),
-        std::pair<float, float>(0.0f, 0.0f)};
+    using Point = std::pair<float, float>;
+    std::vector<Point> c_points_ = {{0.0f, 0.0f},        {-0.4f, -0.59f}, {-0.54f, 0.54f}, {-0.8f, 0.156f},
+                                    {-0.835f, -0.2321f}, {0.0f, 0.8f},    {0.285f, 0.01f}, {0.34f, -0.05f},
+                                    {0.355f, 0.355f},    {0.37f, 0.1f},   {0.0f, 0.0f}};
 
     float animation_progress_ = 0.0f;
     float animation_speed_ = 0.00003f;
 
-    inline void calculate_c_() noexcept;
-    inline float is_in_set_(const float &x, const float &y) const noexcept;
-    inline std::tuple<int, int, int> get_color_(const float &t) const noexcept;
+    void render_grid_() noexcept;
+    void render_sparse_grid() noexcept;
+    void render_sparse_grid_column(int x, float scaled_x) noexcept;
+    bool are_corners_black(int x, int y) noexcept;
+    void fill_edges(int x, int y, float scaled_x_step_div_render_step_width,
+                    float scaled_y_step_div_render_step_width) noexcept;
+    void fill_edge(int x, int y, int i, float scaled_x_step_div_render_step_width,
+                   float scaled_y_step_div_render_step_width) noexcept;
+    void fill_checkerboard_pattern(int x, int y, float scaled_x_step_div_render_step_width,
+                                   float scaled_y_step_div_render_step_width) noexcept;
+    void fill_rest_by_averaging_neighbors(int x, int y) noexcept;
+    float average_of_neighbors(int x, int y, int i, int j) noexcept;
+    void calculate_c_() noexcept;
+    float is_in_set_(const float &x, const float &y) const noexcept;
+    std::tuple<int, int, int> get_color_(const float &t) const noexcept;
+    void draw_rect_(const int &x, const int &y);
 };
 
 inline float fast_log2(float x);
